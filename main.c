c8371cc089868274b3715ed2601de92692d321bf
dboini2 <dboini2@uic.edu>
2025-03-03 06:04:21 +0000

    test

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!");
+    return 0;
+}
+ git config --global user.name "dboini2"
+git config --global user.email dboini2@uic.edu 
+
+
+https://github.com/dboini2/cs211.git
